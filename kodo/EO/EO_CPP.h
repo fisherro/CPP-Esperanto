1f728e118710ed96336b6657748d15c3e80dad43
Rejcx <racheljmorris@gmail.com>
2015-01-16 15:27:27 -0600

    Spaces

--- kodo/EO/EO_CPP.h
+++ kodo/EO/EO_CPP.h
@@ -1,69 +1,69 @@
 #ifndef _EO_CPP
 #define _EO_CPP
 
-#define redoni 		return
-#define cxefejo 	main
-#define uzi 		using
-#define nomspaco 	namespace
+#define redoni      return
+#define cxefejo     main
+#define uzi         using
+#define nomspaco    namespace
 
 /* Control Flow */
-#define se 			if
-#define alie 		else
-#define dum 		while
-#define gxis 		until
-#define fari		do
-#define pro 		for
-#define sxaltilo 	switch
-#define okazo 		case
-#define halti 		break
-#define defauxlto 	default
+#define se          if
+#define alie        else
+#define dum         while
+#define gxis        until
+#define fari        do
+#define pro         for
+#define sxaltilo    switch
+#define okazo       case
+#define halti       break
+#define defauxlto   default
 
 /* Data Types */
-#define entjera 		int
-#define signa 			char
-#define vaka 			void
-#define bulea 			bool
-#define glitpunkta 		float
-#define duobla			double
-#define nesubskribita	unsigned
-#define konstanta		const
-#define statika			static
-#define variema 		volatile
-#define specodeklaro 	typedef
+#define entjera         int
+#define signa           char
+#define vaka            void
+#define bulea           bool
+#define glitpunkta      float
+#define duobla          double
+#define nesubskribita   unsigned
+#define konstanta       const
+#define statika         static
+#define variema         volatile
+#define specodeklaro    typedef
 
 /* Classes and Objects */
-#define klaso		class
-#define virtuala	virtual
-#define amika		friend
-#define publika		public
-#define	privata 	private
-#define protektita	protected
-#define specifado	enum
-#define operacio	operator
-#define strukturo 	struct
-#define sxablono	template
+#define klaso       class
+#define virtuala    virtual
+#define amika       friend
+#define publika     public
+#define privata     private
+#define protektita  protected
+#define specifado   enum
+#define operacio    operator
+#define strukturo   struct
+#define sxablono    template
 
 /* Memory */
-#define forigi		delete
-#define krei		new
+#define forigi      delete
+#define krei        new
 
 /* Other Values */
-#define vera 		true
-#define malvera 	false
+#define vera        true
+#define malvera     false
 
 /* Error Handling */
-#define peni		try
-#define kapti		catch
-#define jxeti 		throw
+#define peni        try
+#define kapti       catch
+#define jxeti       throw
 
 /* stdlib */
-#define konzolaEligo 	cout
-#define konzolaEnigo 	cin
-#define finaLineo 		endl
+#define konzolaEligo    cout
+#define konzolaEnigo    cin
+#define finaLineo       endl
 
-#define	cxena			string
+#define cxena           string
 
-#define dosieraEligo	ofstream
-#define dosieraEnigo	ifstream
+#define dosieraEligo    ofstream
+#define dosieraEnigo    ifstream
 
 #endif
